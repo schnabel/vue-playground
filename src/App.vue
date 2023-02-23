e73b2b9a54785bed52c385ac08454e06fdfe362d
Dirk Habighorst <dirk.habighorst@gmail.com>
2023-02-23 21:10:33 +0100

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import QueryNavbar from './components/QueryNavbar.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <QueryNavbar />
+  <div>
+    <h1>Hello Luna</h1>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://picsum.photos/id/870/600/300" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src="https://picsum.photos/id/871/600/300" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src="https://picsum.photos/id/872/600/300" class="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(bootstrap).mount('#app')
